50e9d4ec2a07fd58bb2690177a2cb6b0344f8ca1
PmpPiyas <princemahmudpiyas@gmail.com>
2026-03-11 22:00:41 +0600

    feat: initial commit

---
+++ src/app/components/shared/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import UpperHeader from '@/app/components/shared/navbar/UpperHeader';
+
+const Navbar = () => {
+  return (
+    <div className="w-full">
+      <UpperHeader />
+    </div>
+  );
+};
+
+export default Navbar;
