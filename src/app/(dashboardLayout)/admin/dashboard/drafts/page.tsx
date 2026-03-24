93dc2ade34ceb66960135f8ce0c11cb359596dec
PmpPiyas <princemahmudpiyas@gmail.com>
2026-03-25 01:32:49 +0600

    build

--- src/app/(dashboardLayout)/admin/dashboard/drafts/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/drafts/page.tsx
@@ -1,6 +1,7 @@
 import DraftsWrapper from '@/app/components/dashboard/admin/drafts/DraftsWrapper';
 import NewsFormSkeleton from '@/app/components/dashboard/admin/drafts/NewsFormSkeleton';
 import { Suspense } from 'react';
+export const dynamic = 'force-dynamic';
 
 const page = () => {
   return (
