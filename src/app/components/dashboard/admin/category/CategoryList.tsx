e45f1fc4d89cb5eb24bdd4f4a248bc8f48c41b98
PmpPiyas <princemahmudpiyas@gmail.com>
2026-04-02 16:42:42 +0600

    add: delete category

--- src/app/components/dashboard/admin/category/CategoryList.tsx
+++ src/app/components/dashboard/admin/category/CategoryList.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import CategoryCard from '@/app/components/dashboard/admin/category/CategoryCard';
-import CategoryHeader from '@/app/components/dashboard/admin/category/CategoryHeader';
+import DeleteConfirmationDialog from '@/app/components/shared/dashboard/DeleteConformationDialog';
+import { ICategory } from '@/interfaces/news.Interface';
+import { deleteCategory } from '@/services/category/delete.category';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-
-interface ICategory {
-  id: string;
-  name: string;
-  description?: string | null;
-}
+import { toast } from 'sonner';
 
 export default function CategoryList({
   categories,
@@ -16,6 +14,32 @@ export default function CategoryList({
   categories: ICategory[];
 }) {
   const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [deletedId, setDeletedId] = useState<string | null>(null);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [editedId, setEditedId] = useState<string | null>(null);
+
+  const router = useRouter();
+
+  const handleDelete = (id: string) => {
+    setDeletedId(id);
+    setIsDeleteDialogOpen(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (!deletedId) return;
+    try {
+      const result = await deleteCategory(deletedId);
+
+      if (result.success) {
+        toast.success(result.message);
+        router.refresh();
+      } else {
+        toast.error(result.message);
+      }
+    } catch (error) {
+      toast.error('Failed to delete category');
+    }
+  };
 
   return (
     <>
@@ -28,9 +52,19 @@ export default function CategoryList({
             description={cat.description}
             isActive={selectedId === cat.id}
             onClick={(id) => setSelectedId(id)}
+            onDelete={handleDelete}
+            onEdit={(id) => setEditedId(id)}
           />
         ))}
       </div>
+
+      <DeleteConfirmationDialog
+        title="Confirm Delete"
+        description="Are you sure you want to delete this category? This action cannot be undone."
+        open={isDeleteDialogOpen}
+        onOpenChange={() => setIsDeleteDialogOpen(!isDeleteDialogOpen)}
+        onConfirm={handleConfirmDelete}
+      />
     </>
   );
 }
